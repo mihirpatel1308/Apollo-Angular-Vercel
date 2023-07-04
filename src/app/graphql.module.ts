3b301f67d9873c13bcc88f42b610f737c45eeae4
mihir.patel <mihir.patel@1rivet.com>
2023-07-04 10:39:48 +0530

    feat: pass access token using configuration in GraphQL request.

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -1,58 +1,52 @@
-import { Apollo, APOLLO_OPTIONS, ApolloModule } from 'apollo-angular';
+import { APOLLO_OPTIONS, ApolloModule } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular/http';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { ApolloLink, InMemoryCache } from '@apollo/client/core';
 import { setContext } from '@apollo/client/link/context';
 import { AuthService } from '@auth0/auth0-angular';
+import { firstValueFrom } from 'rxjs';
 
 const uri = 'https://node-graph-ql-vercel.vercel.app/graphql';
 
-export function createApollo(httpLink: HttpLink) {
-   const basic = setContext((operation, context) => ({
-    headers: {
-      Accept: 'charset=utf-8',
-    },
-  }));
-
-  const auth = setContext((operation, context) => {
-    const token = localStorage.getItem('token');
-    console.log('token:', token);
-
-    if (token === null) {
-      return {};
-    } else {
-      return {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      };
-    }
-  });
-
-  const link = ApolloLink.from([basic, auth, httpLink.create({ uri })]);
-  const cache = new InMemoryCache();
-
-  return {
-    link,
-    cache,
-  };
-}
-
 @NgModule({
-  exports: [HttpClientModule, ApolloModule],
+  imports: [HttpClientModule, ApolloModule],
   providers: [
     {
       provide: APOLLO_OPTIONS,
       useFactory: createApollo,
-      deps: [HttpLink],
+      deps: [HttpLink, AuthService],
     },
   ],
 })
-export class GraphQLModule {
-  constructor(auth: AuthService) {
-    auth.idTokenClaims$.subscribe((claims) => {
-      localStorage.setItem('token', claims!.__raw);
-    });
+export class GraphQLModule { }
+
+export function createApollo(httpLink: HttpLink, authService: AuthService): any {
+  const authMiddleware = setContext(async (_, { headers }) => {
+    const token = await getAccessToken(authService);
+    return {
+      headers: {
+        ...headers,
+        Authorization: token ? `Bearer ${token}` : '',
+      },
+    };
+  });
+
+  const http = httpLink.create({ uri });
+  const link = ApolloLink.from([authMiddleware, http]);
+
+  return {
+    link,
+    cache: new InMemoryCache(),
+  };
+}
+
+async function getAccessToken(authService: AuthService): Promise<string | null> {
+  try {
+    const accessToken = await firstValueFrom(authService.getAccessTokenSilently());
+    return accessToken;
+  } catch (error) {
+    console.error('Error getting access token:', error);
+    return null;
   }
 }
